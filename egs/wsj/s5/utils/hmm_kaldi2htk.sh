d9faf851f50de357d74dae74556cd3cb7a2d555e
songmeixu <suoluomen.smx@gmail.com>
2016-05-11 13:57:21 +0800

    modify hmm_kaldi2qihoo.sh

--- egs/wsj/s5/utils/hmm_kaldi2htk.sh
+++ egs/wsj/s5/utils/hmm_kaldi2htk.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+gmm_dir=exp/dev/tri1
+lang=data/lang
+
 # tree
-tree-conv=/gfs/songmeixu/tools/kaldi-tree-conv/tree-conv
-tree-conv exp/tri1/tree data/lang/phones.txt exp/tri1/questions.int exp/tri1/htk_tree.txt
+tree_conv=/glfs/songmeixu/tools/kaldi-tree-conv/tree-conv
+$(tree_conv) $gmm_dir/tree $lang/phones.txt $gmm_dir/questions.int $gmm_dir/htk_tree.txt
 
 # hmm
-convert2htk data/lang/phones.txt exp/tri1/final.mdl exp/tri1/treeacc exp/tri1/final.occs exp/tri1/tree > exp/tri1/hmmdefs
+convert2htk $lang/phones.txt $gmm_dir/final.mdl $gmm_dir/treeacc $gmm_dir/final.occs $gmm_dir/tree > $gmm_dir/hmmdefs
 
 wait
 echo "Done"
