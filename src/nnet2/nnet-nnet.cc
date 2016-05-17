3883f5bc55ca51466a6e0459b8147390951abcd4
songmeixu <suoluomen.smx@gmail.com>
2016-05-17 12:55:43 +0800

    fix svd bug

--- src/nnet2/nnet-nnet.cc
+++ src/nnet2/nnet-nnet.cc
@@ -724,7 +724,7 @@ void Nnet::LimitRankOfLastLayer(int32 dim) {
 
 void Nnet::LimitRankOfEachLayer(const std::vector<int32> &dimensions) {
   KALDI_ASSERT(dimensions.size() <= NumUpdatableComponents());
-  for (int32 i = components_.size() - 1, d = dimensions.size() - 1; i >= 0 && d >= 0; i--, d--) {
+  for (int32 i = components_.size() - 1, d = dimensions.size() - 1; i >= 0 && d >= 0; i--) {
     AffineComponent *a = NULL, *b = NULL,
         *c = dynamic_cast<AffineComponent*>(components_[i]);
     if (c != NULL && dimensions[d] > 0) {
@@ -734,6 +734,7 @@ void Nnet::LimitRankOfEachLayer(const std::vector<int32> &dimensions) {
       components_.insert(components_.begin() + i + 1, b);
       this->SetIndexes();
       this->Check();
+      --d;
     }
   }
 }
