c9c34dff13039195fed7d2e38c1e4d9f3500c345
songmeixu <suoluomen.smx@gmail.com>
2016-06-02 13:40:39 +0800

    suppress "warning #858: type qualifier on return type is meaningless"

--- src/makefiles/linux_atlas.mk
+++ src/makefiles/linux_atlas.mk
@@ -17,7 +17,7 @@ DOUBLE_PRECISION = 0
 CXXFLAGS = -msse -msse2 -Wall -I.. \
 	   -pthread \
       -DKALDI_DOUBLEPRECISION=$(DOUBLE_PRECISION) \
-      -Wno-sign-compare -Wno-unused-local-typedefs -Winit-self \
+      -Wno-sign-compare -Wno-unused-local-typedefs -Winit-self -Wno-ignored-qualifiers \
       -DHAVE_EXECINFO_H=1 -rdynamic -DHAVE_CXXABI_H \
       -DHAVE_ATLAS -I$(ATLASINC) \
       -I$(FSTROOT)/include \
