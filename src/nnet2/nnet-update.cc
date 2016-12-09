f39c30948895951fce815ec4a80bca9a256027c6
songmeixu <meixu.asr@gmail.com>
2016-12-09 18:46:22 +0800

    test
    Change-Id: Ib29249e349a5839eafd42ccdc4bbad0d3eb4461d

--- src/nnet2/nnet-update.cc
+++ src/nnet2/nnet-update.cc
@@ -195,6 +195,7 @@ double NnetUpdater::ComputeTotAccuracy(
             if (std::find(pdfids_classes[c].begin(), pdfids_classes[c].end(), hyp_pdf_id)
                 != pdfids_classes[c].end()) {
               *tot_classes_accuracy += weight;
+              std::cout << weight << " " << ref_pdf_id << " - " << hyp_pdf_id << "\n";
             }
             break;
           }
