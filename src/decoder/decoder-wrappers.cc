54233be6993869f7c07226f55ff8abd589f80a0d
Meixu Song <meixu.asr@gmail.com>
2018-06-20 14:56:13 +0800

    Revert "fix frame_count"
    This reverts commit 58fa3e23316709f1b15ed302ac7c50e71a3b8d06.

--- src/decoder/decoder-wrappers.cc
+++ src/decoder/decoder-wrappers.cc
@@ -501,7 +501,7 @@ void AlignUtteranceWrapper(
     alignment_writer->Write(utt, alignment);
 
   if (scores_writer != NULL && scores_writer->IsOpen())
-    scores_writer->Write(utt, like / decodable->NumFramesReady());
+    scores_writer->Write(utt, like / *frame_count);
 
   Vector<BaseFloat> per_frame_loglikes;
   if (per_frame_acwt_writer != NULL && per_frame_acwt_writer->IsOpen()) {
