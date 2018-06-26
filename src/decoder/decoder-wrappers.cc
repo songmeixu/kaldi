d98c2fd5f301894c376f6e063b74b944da83c6a4
Meixu Song <meixu.asr@gmail.com>
2018-06-26 11:06:54 +0800

    generate utt mean like/frame in align process

--- src/decoder/decoder-wrappers.cc
+++ src/decoder/decoder-wrappers.cc
@@ -501,7 +501,7 @@ void AlignUtteranceWrapper(
     alignment_writer->Write(utt, alignment);
 
   if (scores_writer != NULL && scores_writer->IsOpen())
-    scores_writer->Write(utt, -(weight.Value1()+weight.Value2()));
+    scores_writer->Write(utt, like / decodable->NumFramesReady());
 
   Vector<BaseFloat> per_frame_loglikes;
   if (per_frame_acwt_writer != NULL && per_frame_acwt_writer->IsOpen()) {
