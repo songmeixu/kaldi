d284ce9fb7e67a6267846da0c04bc7817131de1f
Meixu Song <meixu.asr@gmail.com>
2018-06-28 12:06:33 +0800

    return normally if no file done
    for train_delta continue with existed files

--- src/gmmbin/gmm-acc-stats-ali.cc
+++ src/gmmbin/gmm-acc-stats-ali.cc
@@ -125,7 +125,7 @@ int main(int argc, char *argv[]) {
     if (num_done != 0)
       return 0;
     else
-      return 1;
+      return 0;
   } catch(const std::exception &e) {
     std::cerr << e.what();
     return -1;
